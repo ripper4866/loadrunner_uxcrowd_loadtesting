9eb08a1dcac5a1c45a478c895bea9b99af0f20b0
Nikita Ermolaev <ermolaevnk@yandex.ru>
2020-08-24 11:07:32 +0400

    Merged UC05

--- UC05_video_watch/Action.c
+++ UC05_video_watch/Action.c
@@ -1,26 +1,22 @@
 double videoDuration;
-int videoDurationInteger;
+int videoDurationInteger, i, j;
 
 Action()
 {
 	web_set_sockets_option("SSL_VERSION", "TLS1.2");
-
-	web_reg_save_param_ex(
-		"ParamName=XSRF_TOKEN",
-		"LB/ic=Set-Cookie: XSRF-TOKEN\=",
-		"RB/ic=;",
-		LAST);
-	
-	web_reg_save_param_ex(
-		"ParamName=CSRF_TOKEN",
-		"LB/ic=Set-Cookie: CSRF-TOKEN\=",
-		"RB/ic=;",
-		LAST);
 	
 	// ====================================
-	//	UC05_TR0X_open_mainpage
 	// ====================================
-	lr_start_transaction("UC05_TR0X_open_mainpage");
+	// DEFINE TEST ID HERE!!!
+	lr_save_string("16997737", "testNum");
+//	lr_save_string("16999189", "testNum");
+	// ====================================
+	// ====================================
+	
+	// ====================================
+	//	UC05_TR01_open_mainpage
+	// ====================================
+	lr_start_transaction("UC05_TR01_open_mainpage");
 	
 	// All resources (except "/api/account") are not necessary in script, but can produce load
 	web_url("loadtest.uxcrowd.ru", 
@@ -29,50 +25,10 @@ Action()
 		"Resource=0",
 		"RecContentType=text/html",
 		"Referer=",
-		"Snapshot=t260.inf",
-		"Mode=HTML",
-		EXTRARES,
-		"Url=/api/account", ENDITEM,
-//		"Url=/assets/gulp/sup_css.css", ENDITEM, 
-//		"Url=/assets/css/landing/webflow.css", ENDITEM, 
-//		"Url=/assets/css/landing/uxcrowd.webflow.css", ENDITEM, 
-//		"Url=/assets/css/landing/YouTube.HD.Thumbnail.css", ENDITEM, 
-//		"Url=/assets/gulp/env.js", ENDITEM, 
-//		"Url=/library/jquery.js", ENDITEM, 
-//		"Url=/library/jquery.form.js", ENDITEM, 
-//		"Url=/library/moment.min.js", ENDITEM, 
-//		"Url=/library/jquery.uploadfile.min.js", ENDITEM, 
-//		"Url=/assets/js/main_js/validation.js", ENDITEM, 
-//		"Url=/assets/js/main_js/init.js", ENDITEM, 
-//		"Url=/library/FileSaver.js", ENDITEM, 
-//		"Url=/assets/gulp/sup_js.js", ENDITEM, 
-//		"Url=/assets/js/main_js/validation.rule.js", ENDITEM, 
-//		"Url=https://ulogin.ru/js/ulogin.js", ENDITEM, 
-//		"Url=/assets/js/app_js/auth.interceptor.js", ENDITEM, 
-//		"Url=/assets/gulp/app_js.js", ENDITEM, 
-//		"Url=/assets/js/main_js/mediaelement-and-player.js", ENDITEM, 
-//		"Url=/assets/gulp/admin_js.js", ENDITEM, 
-//		"Url=/assets/gulp/customer_js.js", ENDITEM, 
-//		"Url=/assets/gulp/blog_js.js", ENDITEM, 
-//		"Url=/assets/gulp/home_js.js", ENDITEM, 
-//		"Url=/assets/gulp/moderator_js.js", ENDITEM, 
-//		"Url=/assets/gulp/new_tester_js.js", ENDITEM, 
-//		"Url=/assets/gulp/tester_js.js", ENDITEM, 
-//		"Url=/library/require.js", ENDITEM, 
-//		"Url=/assets/js/main_js/path_controller.js", ENDITEM, 
-//		"Url=/assets/js/landing/jQuery.YouTube.HD.Thumbnail.js", ENDITEM, 
-//		"Url=/assets/js/main_js/main_route.js", ENDITEM, 
-//		"Url=/controller/controller_home/login.controller.js?bust=1597936279196", ENDITEM, 
-//		"Url=/controller/controller_home/newMain.controller.js?bust=1597936279196", ENDITEM, 
-//		"Url=/app.js?bust=1597936279196", ENDITEM, 
+		"Snapshot=t1.inf",
+		"Mode=HTTP",
 		LAST);
 
-	
-	web_add_cookie("XSRF-TOKEN={XSRF_TOKEN}; DOMAIN=loadtest.uxcrowd.ru");
-	web_add_cookie("CSRF-TOKEN={CSRF_TOKEN}; DOMAIN=loadtest.uxcrowd.ru");
-	
-	web_add_auto_header("X-XSRF-TOKEN", "{XSRF_TOKEN}");
-	
 	
 
 	// These HTML partials are not necessary in script, but can produce load
@@ -115,65 +71,192 @@ Action()
 //		"Snapshot=t269.inf",
 //		"Mode=HTML",
 //		LAST);
-	lr_end_transaction("UC05_TR0X_open_mainpage", LR_AUTO);
+	lr_end_transaction("UC05_TR01_open_mainpage", LR_AUTO);
 	// ====================================
-	//	UC05_TR0X_open_mainpage_end
+	//	UC05_TR01_open_mainpage_end
 	// ====================================
+
+	web_reg_save_param_ex(
+		"ParamName=XSRF_TOKEN",
+		"LB/ic=Set-Cookie: XSRF-TOKEN\=",
+		"RB/ic=;",
+		LAST);
 	
+	web_reg_save_param_ex(
+		"ParamName=CSRF_TOKEN",
+		"LB/ic=Set-Cookie: CSRF-TOKEN\=",
+		"RB/ic=;",
+		LAST);
 	// ====================================
-	//	UC05_TR0X_auth_client
+	//	UC05_TR02_auth_client
 	// ====================================
-	lr_start_transaction("UC05_TR0X_auth_client");
+	lr_start_transaction("UC05_TR02_auth_client");
+	
+	web_url("ru.json", 
+		"URL=https://loadtest.uxcrowd.ru/assets/lang/ru.json", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://loadtest.uxcrowd.ru/", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/api/account", ENDITEM, 
+		LAST);
+
+	
+	web_add_cookie("XSRF-TOKEN={XSRF_TOKEN}; DOMAIN=loadtest.uxcrowd.ru");
+	web_add_cookie("CSRF-TOKEN={CSRF_TOKEN}; DOMAIN=loadtest.uxcrowd.ru");
+	
+	web_add_auto_header("X-XSRF-TOKEN", "{XSRF_TOKEN}");
+	
+	web_reg_save_param_regexp(
+		"ParamName=XSRF_TOKEN",
+		"RegExp=XSRF-TOKEN\=(.*?);",
+		"Ordinal=2",
+		LAST);
 	
 	web_submit_data("authentication", 
 		"Action={host}/api/authentication", 
 		"Method=POST", 
 		"Referer={host}/", 
-		"Snapshot=t448.inf", 
+		"Snapshot=t2.inf", 
 		"Mode=HTTP", 
 		ITEMDATA, 
 		"Name=username", "Value={username}", ENDITEM, 
 		"Name=password", "Value={password}", ENDITEM, 
 		"Name=remember-me", "Value=undefined", ENDITEM, 
 		"Name=submit", "Value=Login", ENDITEM, 
+		EXTRARES,
+		"Url=/api/account", ENDITEM,
 		LAST);
 	
-	lr_end_transaction("UC05_TR0X_auth_client", LR_AUTO);
+	lr_end_transaction("UC05_TR02_auth_client", LR_AUTO);
 	// ====================================
-	//	UC05_TR0X_auth_client_end
+	//	UC05_TR02_auth_client_end
 	// ====================================
 	
+	web_revert_auto_header("X-XSRF-TOKEN");
+	web_add_auto_header("X-XSRF-TOKEN", "{XSRF_TOKEN}");
+	
+	
+	//	extracting the number of pages
+	web_reg_save_param_regexp(
+		"ParamName=pagesAll",
+		"RegExp=totalPages\":(.*?),",
+		"Ordinal=1",
+		LAST);
 	
 	// ====================================
-	//	UC05_TR0X_open_orders_page
+	//	UC05_TR03_open_orders
 	// ====================================
-	lr_start_transaction("UC05_TR0X_open_orders_page");
-	
-	web_url("list-orders", 
-		"URL={host}/app-customer-home/list-orders", 
+	lr_start_transaction("UC05_TR03_open_orders");
+
+	web_custom_request("orders", 
+		"URL=https://loadtest.uxcrowd.ru/api/v3/customer/orders", 
+		"Method=POST", 
+		"TargetFrame=", 
 		"Resource=0", 
-		"RecContentType=text/html", 
-		"Referer={host}/", 
-		"Snapshot=t619.inf", 
-		"Mode=HTTP", 
+		"RecContentType=application/json", 
+		"Referer=https://loadtest.uxcrowd.ru/app-customer-home/list-orders", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		"EncType=application/json;charset=UTF-8", 
+		"Body={\"direction\":\"DESC\",\"filter\":[\"ACTIVE\",\"COMPLETED\",\"DRAFT\",\"WAIT_CONFIRMATION\"],\"page\":0,\"size\":10,\"sort\":[\"startDate\"]}", 
 		LAST);
-
-	lr_end_transaction("UC05_TR0X_open_orders_page", LR_AUTO);
+	
+	lr_end_transaction("UC05_TR03_open_orders", LR_AUTO);
 	// ====================================
-	//	UC05_TR0X_open_orders_page_end
+	//	UC05_TR03_open_orders_end
+	// ====================================
+
+
+
+
+	// ====================================
+	//	UC05_TR04_list_orders_pages
+	// ====================================
+	lr_start_transaction("UC05_TR04_list_orders_pages");
+
+	//	flipping pages to the desired value
+	for(i = 0; i < atoi(lr_eval_string("{pagesAll}")); i++) {
+
+		//	forming the request body to go to the next page
+		lr_param_sprintf("bodyOrders", 
+			"{\"direction\":\"DESC\",\"filter\"" 
+			":[\"ACTIVE\",\"COMPLETED\",\"DRAFT\"," 
+			"\"WAIT_CONFIRMATION\"],\"page\":%d" 
+			",\"size\":10,\"sort\":[\"startDate\"]}", 
+			i);
+		
+		//	search for the number of tests per page
+		web_reg_find("Text=testTitle",
+			"SaveCount=testsOnPage",
+			LAST );
+
+		web_custom_request("orders", 
+			"URL=https://loadtest.uxcrowd.ru/api/v3/customer/orders", 
+			"Method=POST", 
+			"TargetFrame=", 
+			"Resource=0", 
+			"RecContentType=application/json", 
+			"Referer=https://loadtest.uxcrowd.ru/app-customer-home/list-orders", 
+			"Snapshot=t25.inf", 
+			"Mode=HTML", 
+			"EncType=application/json;charset=UTF-8", 
+			"Body={bodyOrders}", 
+			LAST);
+
+		//	search for the required test
+		for (j = 1; j <= atoi(lr_eval_string("{testsOnPage}")); j++) {
+			
+			lr_save_int(j, "testCount");
+
+			//	getting all test IDs on the page for comparison with the target ID
+			web_reg_save_param_regexp(
+				"ParamName=testBuffer_{testCount}",
+				"RegExp=id\":(\\d{8}),",
+				"Ordinal={testCount}",
+				LAST);
+		}
+
+		//	sending a request for functions generated by the loop to work
+		web_custom_request("orders", 
+			"URL=https://loadtest.uxcrowd.ru/api/v3/customer/orders", 
+			"Method=POST", 
+			"TargetFrame=", 
+			"Resource=0", 
+			"RecContentType=application/json", 
+			"Referer=https://loadtest.uxcrowd.ru/app-customer-home/list-orders", 
+			"Snapshot=t25.inf", 
+			"Mode=HTML", 
+			"EncType=application/json;charset=UTF-8", 
+			"Body={bodyOrders}", 
+			LAST);
+		
+		//	search for the required test
+		for (j = 1; j <= atoi(lr_eval_string("{testsOnPage}")); j++){
+			
+			lr_param_sprintf("testCheckBuffer", "{testBuffer_%d}", j);
+
+			//	end of the loop when the required test is found
+			if (!strcmp(lr_eval_string(lr_eval_string("{testCheckBuffer}")), lr_eval_string("{testNum}"))) break;
+		}
+		if (!strcmp(lr_eval_string(lr_eval_string("{testCheckBuffer}")), lr_eval_string("{testNum}"))) break;
+	}
+	
+	lr_save_string(lr_eval_string(lr_eval_string("{testCheckBuffer}")), "targetTest");
+
+	lr_end_transaction("UC05_TR04_list_orders_pages", LR_AUTO);
+	// ====================================
+	//	UC05_TR04_list_orders_pages_end
 	// ====================================
 	
-	// ====================================
-	// ====================================
-	// DEFINE TEST ID HERE!!!
-	lr_save_string("16997737", "testNum");
-	// ====================================
-	// ====================================
 
 	// ====================================
-	//	UC05_TR0X_open_test_page
+	//	UC05_TR05_open_test_page
 	// ====================================
-	lr_start_transaction("UC05_TR0X_open_test_page");
+	lr_start_transaction("UC05_TR05_open_test_page");
 	
 	web_url("testPage", 
 		"URL={host}/app-customer-home/tests/{testNum}", 
@@ -184,9 +267,9 @@ Action()
 		"Mode=HTTP", 
 		LAST);
 	
-	lr_end_transaction("UC05_TR0X_open_test_page", LR_AUTO);
+	lr_end_transaction("UC05_TR05_open_test_page", LR_AUTO);
 	// ====================================
-	//	UC05_TR0X_open_test_page_end
+	//	UC05_TR05_open_test_page_end
 	// ====================================
 	
 	web_reg_save_param_regexp(
@@ -196,9 +279,9 @@ Action()
 		LAST);
 	
 	// ====================================
-	//	UC05_TR0X_video_get_id
+	//	UC05_TR06_video_get_id
 	// ====================================
-	lr_start_transaction("UC05_TR0X_video_get_id");
+	lr_start_transaction("UC06_TR0X_video_get_id");
 
 	
 	web_url("testJson", 
@@ -219,9 +302,9 @@ Action()
 		"Mode=HTTP", 
 		LAST);	
 	
-	lr_end_transaction("UC05_TR0X_video_get_id", LR_AUTO);
+	lr_end_transaction("UC06_TR0X_video_get_id", LR_AUTO);
 	// ====================================
-	//	UC05_TR0X_video_get_id_end
+	//	UC05_TR06_video_get_id_end
 	// ====================================
 	
 	
@@ -232,9 +315,9 @@ Action()
 		LAST);
 	
 	// ====================================
-	//	UC05_TR0X_video_get_filename
+	//	UC05_TR07_video_get_filename
 	// ====================================
-	lr_start_transaction("UC05_TR0X_video_get_filename");
+	lr_start_transaction("UC05_TR07_video_get_filename");
 	
 	web_url("videoName", 
 		"URL={host}/api/v3/video/{videoId}", 
@@ -246,29 +329,29 @@ Action()
 		"Mode=HTML", 
 		LAST);
 	
-	lr_end_transaction("UC05_TR0X_video_get_filename", LR_AUTO);
+	lr_end_transaction("UC05_TR07_video_get_filename", LR_AUTO);
 	// ====================================
-	//	UC05_TR0X_video_get_filename_end
+	//	UC05_TR07_video_get_filename_end
 	// ====================================
 	
 	// ====================================
-	//	UC05_TR0X_video_stream_init
+	//	UC05_TR08_video_stream_init
 	// ====================================
-	lr_start_transaction("UC05_TR0X_video_stream_init");
+	lr_start_transaction("UC05_TR08_video_stream_init");
 	
 	web_stream_open("ID=1",
 		"URL={host}/videos/{videoFileName}.mp4",
 		"Protocol=HTTP",
 		"StreamBufferingTimeout=60",
-		//"DumpPath=C:/file.mp4", // debug only, saves the video in storage using path address
+		//"DumpPath=file.mp4", // debug only, saves the video in storage using path address
 		LAST);
 	
 	web_stream_set_param_double("1", SPEED, 1.0);
 	web_stream_set_param_int("1", BUFFERING_TIMEOUT, 60);
 	
-	lr_end_transaction("UC05_TR0X_video_stream_init", LR_AUTO);
+	lr_end_transaction("UC05_TR08_video_stream_init", LR_AUTO);
 	// ====================================
-	//	UC05_TR0X_video_stream_init_end
+	//	UC05_TR08_video_stream_init_end
 	// ====================================
 	
 	videoDuration = web_stream_get_param_double("1", DURATION);
@@ -281,22 +364,22 @@ Action()
 	lr_think_time(15);
 
 	// ====================================
-	//	UC05_TR0X_video_streaming
+	//	UC05_TR09_video_streaming
 	// ====================================
-	lr_start_transaction("UC05_TR0X_video_streaming");
+	lr_start_transaction("UC05_TR09_video_streaming");
 	
 	lr_param_sprintf("videoDurationStreamParam", "PlayingDuration=%d", videoDurationInteger);
 	web_stream_play("ID=1", lr_eval_string("{videoDurationStreamParam}"), "Speed=1", LAST);
 	
 
-	lr_vuser_status_message("Current time in movie: %lf", web_stream_get_param_double("1", CURRENT_TIME));
+	//lr_vuser_status_message("Current time in movie: %lf", web_stream_get_param_double("1", CURRENT_TIME));
 
 	web_stream_stop("ID=1", LAST);
 	web_stream_close("ID=1", LAST);
 
-	lr_end_transaction("UC05_TR0X_video_streaming", LR_AUTO);
+	lr_end_transaction("UC05_TR09_video_streaming", LR_AUTO);
 	// ====================================
-	//	UC05_TR0X_video_streaming_end
+	//	UC05_TR09_video_streaming_end
 	// ====================================
 	
 	return 0;
--- UC05_video_watch/pre_cci.c
+++ UC05_video_watch/pre_cci.c
@@ -2779,11 +2779,82 @@ vuser_init()
 
 # 1 "Action.c" 1
 double videoDuration;
-int videoDurationInteger;
+int videoDurationInteger, i, j;
 
 Action()
 {
 	web_set_sockets_option("SSL_VERSION", "TLS1.2");
+	
+	 
+	 
+	 
+	lr_save_string("16997737", "testNum");
+ 
+	 
+	 
+	
+	 
+	 
+	 
+	lr_start_transaction("UC05_TR01_open_mainpage");
+	
+	 
+	web_url("loadtest.uxcrowd.ru", 
+		"URL={host}/",
+		"TargetFrame=",
+		"Resource=0",
+		"RecContentType=text/html",
+		"Referer=",
+		"Snapshot=t1.inf",
+		"Mode=HTTP",
+		"LAST");
+
+	
+
+	 
+	
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	lr_end_transaction("UC05_TR01_open_mainpage", 2);
+	 
+	 
+	 
 
 	web_reg_save_param_ex(
 		"ParamName=XSRF_TOKEN",
@@ -2796,55 +2867,21 @@ Action()
 		"LB/ic=Set-Cookie: CSRF-TOKEN\=",
 		"RB/ic=;",
 		"LAST");
+	 
+	 
+	 
+	lr_start_transaction("UC05_TR02_auth_client");
 	
-	 
-	 
-	 
-	lr_start_transaction("UC05_TR0X_open_mainpage");
-	
-	 
-	web_url("loadtest.uxcrowd.ru", 
-		"URL={host}/",
-		"TargetFrame=",
-		"Resource=0",
-		"RecContentType=text/html",
-		"Referer=",
-		"Snapshot=t260.inf",
-		"Mode=HTML",
-		"EXTRARES",
-		"Url=/api/account", "ENDITEM",
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+	web_url("ru.json", 
+		"URL=https://loadtest.uxcrowd.ru/assets/lang/ru.json", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://loadtest.uxcrowd.ru/", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		"EXTRARES", 
+		"Url=/api/account", "ENDITEM", 
 		"LAST");
 
 	
@@ -2853,107 +2890,153 @@ Action()
 	
 	web_add_auto_header("X-XSRF-TOKEN", "{XSRF_TOKEN}");
 	
-	
-
-	 
-	
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-	lr_end_transaction("UC05_TR0X_open_mainpage", 2);
-	 
-	 
-	 
-	
-	 
-	 
-	 
-	lr_start_transaction("UC05_TR0X_auth_client");
+	web_reg_save_param_regexp(
+		"ParamName=XSRF_TOKEN",
+		"RegExp=XSRF-TOKEN\=(.*?);",
+		"Ordinal=2",
+		"LAST");
 	
 	web_submit_data("authentication", 
 		"Action={host}/api/authentication", 
 		"Method=POST", 
 		"Referer={host}/", 
-		"Snapshot=t448.inf", 
+		"Snapshot=t2.inf", 
 		"Mode=HTTP", 
 		"ITEMDATA", 
 		"Name=username", "Value={username}", "ENDITEM", 
 		"Name=password", "Value={password}", "ENDITEM", 
 		"Name=remember-me", "Value=undefined", "ENDITEM", 
 		"Name=submit", "Value=Login", "ENDITEM", 
+		"EXTRARES",
+		"Url=/api/account", "ENDITEM",
 		"LAST");
 	
-	lr_end_transaction("UC05_TR0X_auth_client", 2);
+	lr_end_transaction("UC05_TR02_auth_client", 2);
 	 
 	 
 	 
 	
+	(web_remove_auto_header("X-XSRF-TOKEN", "ImplicitGen=Yes", "LAST"));
+	web_add_auto_header("X-XSRF-TOKEN", "{XSRF_TOKEN}");
+	
+	
+	 
+	web_reg_save_param_regexp(
+		"ParamName=pagesAll",
+		"RegExp=totalPages\":(.*?),",
+		"Ordinal=1",
+		"LAST");
 	
 	 
 	 
 	 
-	lr_start_transaction("UC05_TR0X_open_orders_page");
-	
-	web_url("list-orders", 
-		"URL={host}/app-customer-home/list-orders", 
+	lr_start_transaction("UC05_TR03_open_orders");
+
+	web_custom_request("orders", 
+		"URL=https://loadtest.uxcrowd.ru/api/v3/customer/orders", 
+		"Method=POST", 
+		"TargetFrame=", 
 		"Resource=0", 
-		"RecContentType=text/html", 
-		"Referer={host}/", 
-		"Snapshot=t619.inf", 
-		"Mode=HTTP", 
+		"RecContentType=application/json", 
+		"Referer=https://loadtest.uxcrowd.ru/app-customer-home/list-orders", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		"EncType=application/json;charset=UTF-8", 
+		"Body={\"direction\":\"DESC\",\"filter\":[\"ACTIVE\",\"COMPLETED\",\"DRAFT\",\"WAIT_CONFIRMATION\"],\"page\":0,\"size\":10,\"sort\":[\"startDate\"]}", 
 		"LAST");
+	
+	lr_end_transaction("UC05_TR03_open_orders", 2);
+	 
+	 
+	 
 
-	lr_end_transaction("UC05_TR0X_open_orders_page", 2);
+
+
+
+	 
+	 
+	 
+	lr_start_transaction("UC05_TR04_list_orders_pages");
+
+	 
+	for(i = 0; i < atoi(lr_eval_string("{pagesAll}")); i++) {
+
+		 
+		lr_param_sprintf("bodyOrders", 
+			"{\"direction\":\"DESC\",\"filter\"" 
+			":[\"ACTIVE\",\"COMPLETED\",\"DRAFT\"," 
+			"\"WAIT_CONFIRMATION\"],\"page\":%d" 
+			",\"size\":10,\"sort\":[\"startDate\"]}", 
+			i);
+		
+		 
+		web_reg_find("Text=testTitle",
+			"SaveCount=testsOnPage",
+			"LAST" );
+
+		web_custom_request("orders", 
+			"URL=https://loadtest.uxcrowd.ru/api/v3/customer/orders", 
+			"Method=POST", 
+			"TargetFrame=", 
+			"Resource=0", 
+			"RecContentType=application/json", 
+			"Referer=https://loadtest.uxcrowd.ru/app-customer-home/list-orders", 
+			"Snapshot=t25.inf", 
+			"Mode=HTML", 
+			"EncType=application/json;charset=UTF-8", 
+			"Body={bodyOrders}", 
+			"LAST");
+
+		 
+		for (j = 1; j <= atoi(lr_eval_string("{testsOnPage}")); j++) {
+			
+			lr_save_int(j, "testCount");
+
+			 
+			web_reg_save_param_regexp(
+				"ParamName=testBuffer_{testCount}",
+				"RegExp=id\":(\\d{8}),",
+				"Ordinal={testCount}",
+				"LAST");
+		}
+
+		 
+		web_custom_request("orders", 
+			"URL=https://loadtest.uxcrowd.ru/api/v3/customer/orders", 
+			"Method=POST", 
+			"TargetFrame=", 
+			"Resource=0", 
+			"RecContentType=application/json", 
+			"Referer=https://loadtest.uxcrowd.ru/app-customer-home/list-orders", 
+			"Snapshot=t25.inf", 
+			"Mode=HTML", 
+			"EncType=application/json;charset=UTF-8", 
+			"Body={bodyOrders}", 
+			"LAST");
+		
+		 
+		for (j = 1; j <= atoi(lr_eval_string("{testsOnPage}")); j++){
+			
+			lr_param_sprintf("testCheckBuffer", "{testBuffer_%d}", j);
+
+			 
+			if (!strcmp(lr_eval_string(lr_eval_string("{testCheckBuffer}")), lr_eval_string("{testNum}"))) break;
+		}
+		if (!strcmp(lr_eval_string(lr_eval_string("{testCheckBuffer}")), lr_eval_string("{testNum}"))) break;
+	}
+	
+	lr_save_string(lr_eval_string(lr_eval_string("{testCheckBuffer}")), "targetTest");
+
+	lr_end_transaction("UC05_TR04_list_orders_pages", 2);
 	 
 	 
 	 
 	
-	 
-	 
-	 
-	lr_save_string("16997737", "testNum");
-	 
-	 
 
 	 
 	 
 	 
-	lr_start_transaction("UC05_TR0X_open_test_page");
+	lr_start_transaction("UC05_TR05_open_test_page");
 	
 	web_url("testPage", 
 		"URL={host}/app-customer-home/tests/{testNum}", 
@@ -2964,7 +3047,7 @@ Action()
 		"Mode=HTTP", 
 		"LAST");
 	
-	lr_end_transaction("UC05_TR0X_open_test_page", 2);
+	lr_end_transaction("UC05_TR05_open_test_page", 2);
 	 
 	 
 	 
@@ -2978,7 +3061,7 @@ Action()
 	 
 	 
 	 
-	lr_start_transaction("UC05_TR0X_video_get_id");
+	lr_start_transaction("UC06_TR0X_video_get_id");
 
 	
 	web_url("testJson", 
@@ -2999,7 +3082,7 @@ Action()
 		"Mode=HTTP", 
 		"LAST");	
 	
-	lr_end_transaction("UC05_TR0X_video_get_id", 2);
+	lr_end_transaction("UC06_TR0X_video_get_id", 2);
 	 
 	 
 	 
@@ -3014,7 +3097,7 @@ Action()
 	 
 	 
 	 
-	lr_start_transaction("UC05_TR0X_video_get_filename");
+	lr_start_transaction("UC05_TR07_video_get_filename");
 	
 	web_url("videoName", 
 		"URL={host}/api/v3/video/{videoId}", 
@@ -3026,7 +3109,7 @@ Action()
 		"Mode=HTML", 
 		"LAST");
 	
-	lr_end_transaction("UC05_TR0X_video_get_filename", 2);
+	lr_end_transaction("UC05_TR07_video_get_filename", 2);
 	 
 	 
 	 
@@ -3034,19 +3117,19 @@ Action()
 	 
 	 
 	 
-	lr_start_transaction("UC05_TR0X_video_stream_init");
+	lr_start_transaction("UC05_TR08_video_stream_init");
 	
 	web_stream_open("ID=1",
 		"URL={host}/videos/{videoFileName}.mp4",
 		"Protocol=HTTP",
 		"StreamBufferingTimeout=60",
-		"DumpPath=C:/file.mp4",  
+		 
 		"LAST");
 	
 	web_stream_set_param_double("1", 13, 1.0);
 	web_stream_set_param_int("1", 12, 60);
 	
-	lr_end_transaction("UC05_TR0X_video_stream_init", 2);
+	lr_end_transaction("UC05_TR08_video_stream_init", 2);
 	 
 	 
 	 
@@ -3063,18 +3146,18 @@ Action()
 	 
 	 
 	 
-	lr_start_transaction("UC05_TR0X_video_streaming");
+	lr_start_transaction("UC05_TR09_video_streaming");
 	
 	lr_param_sprintf("videoDurationStreamParam", "PlayingDuration=%d", videoDurationInteger);
 	web_stream_play("ID=1", lr_eval_string("{videoDurationStreamParam}"), "Speed=1", "LAST");
 	
 
-	lr_vuser_status_message("Current time in movie: %lf", web_stream_get_param_double("1", 2));
+	 
 
 	web_stream_stop("ID=1", "LAST");
 	web_stream_close("ID=1", "LAST");
 
-	lr_end_transaction("UC05_TR0X_video_streaming", 2);
+	lr_end_transaction("UC05_TR09_video_streaming", 2);
 	 
 	 
 	 
